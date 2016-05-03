c91a4d23665904384492e6fb37f3ac304ee024ed
Kevin Phillips <kphillips86@gmail.com>
2016-05-02 22:25:06 -0500

    installing dependencies, updating main test files, making template files consistent, and adding tests

--- default/templates/static/supermodel/model_test.js
+++ default/templates/static/supermodel/model_test.js
@@ -2,12 +2,12 @@ import 'steal-mocha';
 import assert from 'assert';
 import <%= className %> from './<%= name %>';
 
-describe('models/<%= name %>', () => {
-  it('getList', function(done) {
+describe('models/<%= name %>', function() {
+  it('getList', function(done){
     <%= className %>.getList().then(function(items) {
       assert.equal(items.length, 2);
       assert.equal(items.attr('0.description'), 'First item');
       done();
-    }, done);
+    });
   });
 });
